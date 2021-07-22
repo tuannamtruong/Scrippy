5775a5c6bc0c90ddf7b63c95205106f04c7d8baf
tuan nam <ttnam1995@gmail.com>
2021-07-22 17:10:59 +0200

    Update CreateNewWebSln.sh

--- CreateNewWebSln.sh
+++ CreateNewWebSln.sh
@@ -9,18 +9,20 @@ libMngrVer="2.0.96"
 bootstrapVer="twitter-bootstrap@4.3.1"
 moqVersion="4.13.1"
 efCoreVer="5.0.7"
+
 # Create solution and web project
-dotnet new globaljson --sdk-version %sdkVer% --output $slnName/$projectName
-dotnet new web --no-https --output $slnName/$projectName.Web --framework $frameworkVer
+dotnet new globaljson --sdk-version %sdkVer% --output $slnName/$projectName.Web
+dotnet new mvc --no-https --output $slnName/$projectName.Web --framework $frameworkVer
 dotnet new sln -o $slnName
 dotnet sln $slnName add $slnName/$projectName.Web
 echo "Finish solution and web project creation"
 
 # Add libraries for web project
-cd $slnName/$projectName
+cd $slnName/$projectName.Web
 dotnet tool install --global Microsoft.Web.LibraryManager.Cli --version $libMngrVer
 libman init -p cdnjs
 libman install $bootstrapVer -d wwwroot/lib/bootstrap
+cd ../..
 echo "Finish addinng libraries for web project"
 
 # Create test project
@@ -30,11 +32,11 @@ dotnet add $slnName/$projectName.Tests reference $slnName/$projectName.Web
 echo "Finish test project creation"
 
 # Add libraries for test project
-dotnet add $slnName/$projectName.Tests package Moq --version 
+dotnet add $slnName/$projectName.Tests package Moq --version $moqVersion
 echo "Finish addinng libraries for test project"
 
 # Create data provider project
-dotnet new xunit -o $slnName/$projectName.DataProvider --framework $frameworkVer
+dotnet new classlib -o $slnName/$projectName.DataProvider --framework $frameworkVer
 dotnet sln $slnName add $slnName/$projectName.DataProvider
 echo "Finish data provider project creation"
 
